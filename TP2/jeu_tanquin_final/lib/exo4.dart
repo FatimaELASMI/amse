99c7b78bce0dbb3ec46c685d703c2e876b1d1e4f
FatimaELASMI <fatimazehraeelasmi@gmail.com>
2023-02-27 15:02:08 +0100

    changement à une version stable de flutter et creation de nouveau projet

---
+++ TP2/jeu_tanquin_final/lib/exo4.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class Tile {
+  final String imageURL;
+  final Alignment alignment;
+
+  const Tile({required this.imageURL, required this.alignment});
+
+  Widget croppedImageTile() {
+    return FittedBox(
+      fit: BoxFit.fill,
+      child: ClipRect(
+        child: Container(
+          child: Align(
+            alignment: this.alignment,
+            widthFactor: 0.3,
+            heightFactor: 0.3,
+            child: Image.asset(this.imageURL),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Tile tile = new Tile(
+    imageURL: 'assets/images/flutter.png', alignment: Alignment(0, 0));
+
+class DisplayTileWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Display a Tile as a Cropped Image'),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: Column(children: [
+        SizedBox(
+            width: 150.0,
+            height: 150.0,
+            child: Container(
+                margin: EdgeInsets.all(20.0),
+                child: this.createTileWidgetFrom(tile))),
+        Container(
+            height: 200,
+            child: Image.asset('assets/images/flutter.png',
+                fit: BoxFit.cover))
+      ])),
+    );
+  }
+
+  Widget createTileWidgetFrom(Tile tile) {
+    return InkWell(
+      child: tile.croppedImageTile(),
+      onTap: () {
+        print("tapped on tile");
+      },
+    );
+  }
+}
