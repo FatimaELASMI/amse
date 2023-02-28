03a6d61b399b27edad3dd554016513520267c75f
FatimaELASMI <fatimazehraeelasmi@gmail.com>
2023-02-28 16:53:26 +0100

    générer des couleurs aléatoires pour les tuiles de l'exo 5

--- TP2/jeu_tanquin_final/lib/exo5a.dart
+++ TP2/jeu_tanquin_final/lib/exo5a.dart
@@ -1,51 +1,46 @@
+import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'dart:ui';
-
 
 class DisplayGridView extends StatelessWidget {
-List<Image> images = [
-  Image.asset('assets/images/paris.jpg'),
-];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Tile Grid View'),
       ),
-// partie 1 : les portions sont des simples containers
-
       body: GridView.count(
         padding: const EdgeInsets.all(20),
-         crossAxisSpacing: 4,
-            mainAxisSpacing: 4,
-        crossAxisCount: 3, 
+        crossAxisSpacing: 4,
+        mainAxisSpacing: 4,
+        crossAxisCount: 3,
         children: List.generate(
-          9, 
-          (index) => Container(
-            padding: const EdgeInsets.all(8),
-            margin: EdgeInsets.all(2.0), // marge autour de la tuile
-            color: Colors.blueGrey, // couleur de fond de la tuile
-            child: Center(
-              child: Text(
-                'Tile ${index + 1}', 
-                style: TextStyle(
-                  color: Colors.white, 
-                  fontSize: 24.0, 
-                  fontWeight: FontWeight.bold, 
+          9,
+          (index) {
+            // générer des couleurs aléatoires pour les tuiles
+            final random = Random();
+            final r = random.nextInt(256);
+            final g = random.nextInt(256);
+            final b = random.nextInt(256);
+            final color = Color.fromARGB(255, r, g, b);
+            return Container(
+              padding: const EdgeInsets.all(8),
+              margin: EdgeInsets.all(2.0),
+              color: color,
+              child: Center(
+                child: Text(
+                  'Tile ${index + 1}',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-          ),
+            );
+          },
         ),
       ),
-
-        );
-     
+    );
   }
-
-  
- 
 }
-
